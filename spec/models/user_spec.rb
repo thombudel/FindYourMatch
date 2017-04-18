97aec00e8851f330062d147af8a1452626a8d5e4
thom budel <thombudel@gmail.com>
2017-04-18 20:43:50 +0200

    Added tests for user validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "user sign in/up validations" do
+    it "is invalid without an email address" do
+      user = User.new(email: "")
+      user.valid?
+      expect(user.errors).to have_key(:email)
+    end
+
+    it "is invalid without a password" do
+      user = User.new(password: "")
+      user.valid?
+      expect(user.errors).to have_key(:password)
+    end
+
+    it "is invalid with a password short than 6 characters" do
+      user = User.new(password: "Hey1")
+      user.valid?
+      expect(user.errors).to have_key(:password)
+    end
+  end
 end

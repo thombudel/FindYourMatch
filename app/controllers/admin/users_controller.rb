f65bdeb37a75f6578a179d88a05ebcd97bacb44d
thom budel <thombudel@gmail.com>
2017-04-19 16:36:48 +0200

    Added student/admin boolean change buttons

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,18 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to :admin_users, notice: "User updated"
+    else
+      render 'index'
+    end
+  end
 end
+
+private
+  def user_params
+    params.permit(:email, :admin)
+  end

f9d1d1de69c5f300c5104c8a400292bcb507cd0c
thom budel <thombudel@gmail.com>
2017-04-20 08:18:15 +0200

    Added authorization method students controller

--- app/controllers/student/students_controller.rb
+++ app/controllers/student/students_controller.rb
@@ -1,6 +1,14 @@
 class Student::StudentsController < ApplicationController
   before_action :authenticate_user!
+  before_filter :authorized?
 
   def home
+  end
+
+  def authorized?
+    if current_user.admin == false
+    else
+      redirect_to root_path
+    end
   end
 end

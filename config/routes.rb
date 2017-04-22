5f8de7c9a7750d04bf2fc0949cba143182faf890
thom budel <thombudel@gmail.com>
2017-04-22 13:39:40 +0200

    Adjusted routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # get '/admin', to: 'admin#home'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
 
   namespace :admin do

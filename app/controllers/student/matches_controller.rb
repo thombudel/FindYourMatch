a4b5c5029bd818dd4e46e6b0b4e27a71ecaef30a
thom budel <thombudel@gmail.com>
2017-04-22 14:00:48 +0200

    Added previous matches in seeds for student Iris

--- app/controllers/student/matches_controller.rb
+++ app/controllers/student/matches_controller.rb
@@ -2,6 +2,12 @@ class Student::MatchesController < ApplicationController
   before_action :authenticate_user!
   before_filter :authorized?
 
+  def index
+    @matches = Match
+    .where(date: Date.today)
+  end
+
+
   def authorized?
     if current_user.admin == false
     else

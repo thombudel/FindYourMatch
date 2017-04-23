643c0537551e435359e1baca2ad3ea541ae1d898
thom budel <thombudel@gmail.com>
2017-04-23 12:57:09 +0200

    Feature test listing all users

---
+++ spec/features/list_users_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "list of all users" do
+  before { login_as admin }
+
+  let(:admin) { create :user, admin: true }
+  let!(:student_1) { create :user }
+  let!(:student_2) { create :user }
+
+  it "lists all signed up users and shows their name and email" do
+    visit admin_users_url
+    expect(page).to have_text(student_1.name)
+    expect(page).to have_text(student_2.name)
+    expect(page).to have_text(student_1.email)
+    expect(page).to have_text(student_2.email)
+  end
+end

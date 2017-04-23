3478530a0ec490ce832c1b4e91fbe42ff7247a0d
thom budel <thombudel@gmail.com>
2017-04-23 11:30:33 +0200

    refactor match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -2,17 +2,14 @@ class Match < ApplicationRecord
   belongs_to :student_1, :class_name => 'User'
   belongs_to :student_2, :class_name => 'User'
 
+  @all_students = User.where(admin: false).order("RANDOM()")
 
   def self.create_matches
-    students = User.where(admin: false).order("RANDOM()")
-
-    unmatched_students = students.select do |student|
-      !student.has_match?
+    available_students = @all_students.select do |student|
+        !student.has_match?
     end
-
-    Match.create(student_1: unmatched_students.first,
-    student_2: unmatched_students.second,
-    date: Date.today
-    )
+    Match.create(student_1: available_students.first,
+    student_2: available_students.second,
+    date: Date.today)
   end
 end

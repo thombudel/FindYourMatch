a76f3277a20728307fc5f08722591263abeba46d
thom budel <thombudel@gmail.com>
2017-04-23 19:05:26 +0200

    Minor changes

--- app/models/match.rb
+++ app/models/match.rb
@@ -4,10 +4,17 @@ class Match < ApplicationRecord
 
   @all_students = User.where(admin: false).order("RANDOM()")
 
-  def self.create_matches
-    available_students = @all_students.select do |student|
-        !student.has_match?
+  def self.previous_matches
+    @previous_matches = Array.new
+    all.each do |match|
+      @previous_matches << [match.student_1, match.student_2]
     end
+    return @previous_matches
+  end
+
+  def self.create_matches
+    available_students = @all_students.select { |student| !student.has_match? }
+
     Match.create(student_1: available_students.first,
     student_2: available_students.second,
     date: Date.today)

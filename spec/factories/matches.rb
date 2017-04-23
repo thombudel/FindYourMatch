afce4c2a29ec078d6f25bcc4cfcfc285de5e8263
thom budel <thombudel@gmail.com>
2017-04-23 12:59:12 +0200

    Added matches factory

--- spec/factories/matches.rb
+++ spec/factories/matches.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :match do
-    date "2017-04-19"
-    student_1 1
-    student_2 1
+    date        { Date.today }
+    student_1   { build(:user)}
+    student_2   { build(:user)}
   end
 end
